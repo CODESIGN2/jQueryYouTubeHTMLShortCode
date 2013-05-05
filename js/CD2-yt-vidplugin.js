74c61a94c81ab6c747ffce576f098a63cdbf4078
Lewiscowles1986 <lewis-cowles@codesign2.co.uk>
2013-05-05 14:38:33 +0200

    Updated to support aspect-ratio
    Updated to support aspect-ratio, now allows to set video to comply with original video aspect ratio and scale by ratio.

--- js/CD2-yt-vidplugin.js
+++ js/CD2-yt-vidplugin.js
@@ -1,8 +1,37 @@
 jQuery(document).ready(function(){
+	window.extractYTVid = function(elem) {
+		var aspect = extractAspectRatio( elem, jQuery(elem).data('aspectratio') );
+		var dimensions = arToWH(elem,aspect)
+		var vid = jQuery(elem).attr('id');
+		jQuery(elem).html( '<iframe width="'+dimensions.width+'" height="'+dimensions.height+'" src="http://www.youtube-nocookie.com/embed/'+vid+'" frameborder="0" allowfullscreen></iframe>' );
+	};
+	window.extractAspectRatio = function( elem , arRaw ) { // returns the aspect ratio
+		var defWidth = 100; // Default width percentage
+		var arData = [16,9]; // default to a 16:9 ratio
+		if( typeof(arRaw) != 'undefined' ) {
+			if ( arRaw.indexOf(':') >= 0 ) {
+				arData = arRaw.split(':');
+			}
+		}
+		var arObj = new Object();
+		arObj.x = arData[0]; arObj.y = arData[1];
+		return arObj;
+	};
+	window.arToWH = function( elem , ar ) { // returns the width & height based on aspect ratio
+		var eWidth = parseFloat(jQuery(elem).width() );
+		var eHeight = parseFloat( jQuery(elem).height() );
+		if(eHeight > 1 && eWidth < 1){ // height set, width not set (generally overwrite with explicit css width:0px;)
+			eWidth = eHeight / ar.y * ar.x;
+		} else if(eHeight < 1 && eWidth < 1) { // if width & height set
+			eWidth = defWidth+'%'; eHeight = ( ( defWidth/ar.x )*ar.y )+'%';
+		} else { // if width set and not height
+			eHeight = eWidth / ar.x * ar.y;
+		}
+		var obj = {};
+		obj.width = eWidth; obj.height = eHeight;
+		return obj;
+	}
 	jQuery('.youtubevid').each(function(){
-		var vid = jQuery(this).attr('id');
-		var width = jQuery(this).attr('width');
-		var height = jQuery(this).attr('height');
-		jQuery(this).html('<iframe width="'+width+'" height="'+height+'" src="http://www.youtube-nocookie.com/embed/'+vid+'" frameborder="0" allowfullscreen></iframe>');
+		extractYTVid(this);
 	});
 });
